8353dae878fa9d292f14e24153fde745a0ec4ad5
RedMaun <deb.il.dpkg@gmail.com>
2022-10-24 03:20:02 +0700

    1

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const resp = await fetch('/dict.json')
 const dictionary = await resp.json()
+
+let url = new URL(window.location.href).searchParams.get('subj')
+if (url)
+{
+    Object.entries(dictionary).map((s, index) => {
+        if (url === s[0]) {
+            localStorage.setItem('page', `${index}`); window.open('page.html', "_self")
+        }
+    })
+}
+
 document.getElementById('dictionary').innerHTML = ''.concat(...Object.entries(dictionary).map((s, index) => `<li class="cont" id="${index}""><div class="description">${s[0]} - ${s[1]["meaning"]}</div><i class="arrow fa-solid fa-arrow-right fa-lg"></i></li>`))
 
 let conts = document.getElementsByClassName('cont')
